0c626ebcbd8c555aa2b31d3c30b6312f4db40273
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-09-23 10:13:09 +0300

    Кода коммутаций заменены с 20ХХ на 30ХХ; MDFA и MDF2 заменены на MDFA\MDF2-DD и MDFA\MDF2-SD; Кода коммутаций 10ХХ заменены на 31ХХ;

--- Firmware/Source/Controller/Controller.c
+++ Firmware/Source/Controller/Controller.c
@@ -393,7 +393,7 @@ bool CONTROL_CheckContactors(DevType DevCase, Int16U ActionID, Int16U DUTPositio
 					return CONTROL_CheckContactorsStates_macro(((DUTPosition == DUT_POS1) ? CT_Ices_MISM2_CH_1 : CT_Ices_MISM2_CH_2));
 					break;
 
-				case SC_Type_MDF2:
+				case SC_Type_MDFA_MDF2_SD:
 				case SC_Type_MDA2:
 					return CONTROL_CheckContactorsStates_macro(CT_Ices_Pos2_Inverse);
 
@@ -420,11 +420,11 @@ bool CONTROL_CheckContactors(DevType DevCase, Int16U ActionID, Int16U DUTPositio
 					return CONTROL_CheckContactorsStates_macro(((DUTPosition == DUT_POS1) ? CT_Vcesat_MISM2_CH_1 : CT_Vcesat_MISM2_CH_2));
 					break;
 
-				case SC_Type_MDF2:
+				case SC_Type_MDFA_MDF2_SD:
 				case SC_Type_MDA2:
 				case SC_Type_MDSV:
 				case SC_Type_MDSM:
-				case SC_Type_MDFA:
+				case SC_Type_MDFA_MDF2_DD:
 				case SC_Type_MDAA:
 					return CONTROL_CheckContactorsStates_macro(CT_NO_PE);
 					break;
@@ -453,7 +453,7 @@ bool CONTROL_CheckContactors(DevType DevCase, Int16U ActionID, Int16U DUTPositio
 					return CONTROL_CheckContactorsStates_macro(((DUTPosition == DUT_POS1) ? CT_Vf_MISM2_CH_1 : CT_Vf_MISM2_CH_2));
 					break;
 
-				case SC_Type_MDF2:
+				case SC_Type_MDFA_MDF2_SD:
 				case SC_Type_MDA2:
 					return CONTROL_CheckContactorsStates_macro(CT_Vcesat_Pos2);
 
@@ -480,11 +480,11 @@ bool CONTROL_CheckContactors(DevType DevCase, Int16U ActionID, Int16U DUTPositio
 					return CONTROL_CheckContactorsStates_macro(((DUTPosition == DUT_POS1) ? CT_Qg_MISM2_CH_1 : CT_Qg_MISM2_CH_2));
 					break;
 
-				case SC_Type_MDF2:
+				case SC_Type_MDFA_MDF2_SD:
 				case SC_Type_MDA2:
 				case SC_Type_MDSV:
 				case SC_Type_MDSM:
-				case SC_Type_MDFA:
+				case SC_Type_MDFA_MDF2_DD:
 				case SC_Type_MDAA:
 					return CONTROL_CheckContactorsStates_macro(CT_NO_PE);
 					break;
